11689cb0627d3742db6eb83c43ca513db65f293a
Minhaj Ahmad Khan <51471123+minhajspace@users.noreply.github.com>
2026-05-11 13:45:28 +0530

    fix build error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   transpilePackages: ["antd", "@ant-design/icons"],
   serverExternalPackages: ["@sparticuz/chromium"],
+  outputFileTracingIncludes: {
+    "/api/barcode/export-pdf": ["node_modules/@sparticuz/chromium/bin/**/*"],
+  },
   poweredByHeader: false,
   async headers() {
     return [
