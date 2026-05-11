19b16b5847098d926b23228b8479f3c96d57c57c
Minhaj Ahmad Khan <51471123+minhajspace@users.noreply.github.com>
2026-05-11 13:36:11 +0530

    fix build error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["antd", "@ant-design/icons"],
+  serverExternalPackages: ["@sparticuz/chromium"],
   poweredByHeader: false,
   async headers() {
     return [
